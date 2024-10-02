d2cb612c74055c0b3869769bdfcc904bc71e0eab
yongilkim01 <yongil.k01@gmail.com>
2024-10-02 20:16:38 +0900

    feat: add item, weapon class
    - AItem class
     * 아이템 관련 클래스들의 가장 상위 부모 클래스.
    
    - ABaseWeapon class
     * AItem 클래스를 상속받은 무기 관련 클래스들의 부모 클래스.
    
    - APlayerWeapon class
     * 플레이어가 사용하는 무기의 기능을 구현하는 클래스.
    
    Authored-by : Yongil Kim <yongil.k01@gmail.com>

---
+++ Source/Valhalla/Public/Items/Weapons/BaseWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "BaseWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALHALLA_API ABaseWeapon : public AItem
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Valhalla/Public/Items/Weapons/PlayerWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/BaseWeapon.h"
+#include "PlayerWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALHALLA_API APlayerWeapon : public ABaseWeapon
+{
+	GENERATED_BODY()
+	
+};
